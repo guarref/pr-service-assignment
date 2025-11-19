37a200e54e888f73bdc491cce7e238c0b0d26f09
guarref <prostoboh902@gmail.com>
2025-11-20 01:13:23 +0300

    new repository

--- go.mod
+++ go.mod
@@ -1,9 +1,3 @@
-module github.com/guarref/prservice-task
+module github.com/guarref/pr-service-assignment
 
 go 1.23.2
-
-require (
-	github.com/golang-migrate/migrate/v4 v4.19.0 // indirect
-	github.com/hashicorp/errwrap v1.1.0 // indirect
-	github.com/hashicorp/go-multierror v1.1.1 // indirect
-)
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,7 +3,7 @@ package repository
 import (
 	"context"
 
-	"github.com/guarref/prservice-task/internal/domain"
+	"github.com/guarref/pr-service-assignment/internal/domain"
 )
 
 type TeamRepository interface {
@@ -13,14 +13,11 @@ type TeamRepository interface {
 
 type UserRepository interface {
 	SetFlagIsActive(ctx context.Context, userID string, isActive bool) error
-	// CreateOrUpdateUser(ctx context.Context, user *domain.User) error
-	// GetUserByID(ctx context.Context, userID string) (*domain.User, error)
 }
 
 type PullRequestRepository interface {
 	CreatePullRequest(ctx context.Context, pr *domain.PullRequest) error
-	GetPullRequestByID(ctx context.Context, prID string) (*domain.PullRequest, error)
-	UpdatePullRequest(ctx context.Context, pr *domain.PullRequest) error
+	MergePullRequestByID(ctx context.Context, prID string) (*domain.PullRequest, error)
+	ReassignToPullRequest(ctx context.Context, prID string, userID string) (*domain.PullRequest, error)
 	GetPullRequestByReviewerID(ctx context.Context, userID string) ([]*domain.PullRequest, error)
 }
-
