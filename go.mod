8dcfa5e81fbb98621d6da755db33d5131a275468
guarref <prostoboh902@gmail.com>
2025-11-20 03:06:31 +0300

    add config and postgres

--- go.mod
+++ go.mod
@@ -1,3 +1,22 @@
 module github.com/guarref/pr-service-assignment
 
 go 1.23.2
+
+require (
+	github.com/caarlos0/env/v6 v6.10.1
+	github.com/golang-migrate/migrate/v4 v4.19.0
+	github.com/jackc/pgx/v5 v5.5.4
+	github.com/jmoiron/sqlx v1.4.0
+)
+
+require (
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/sync v0.12.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+)
---
+++ pkg/postgres/postgres.go
@@ -0,0 +1,67 @@
+package postgres
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+
+	_ "github.com/golang-migrate/migrate/v4/source/file" // источник миграций file://
+	_ "github.com/jackc/pgx/v5/stdlib"                   // регистрирует драйвер pgx для database/sql
+
+	"github.com/jmoiron/sqlx"
+)
+
+
+type PDB struct {
+	DB *sqlx.DB
+}
+
+func NewPDB(dsn string) (*PDB, error) {
+
+	db, err := sqlx.Connect("pgx", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("postgres: failed to connect: %w", err)
+	}
+
+	sqlDB := db.DB
+	sqlDB.SetMaxOpenConns(10)
+	sqlDB.SetMaxIdleConns(5)
+	sqlDB.SetConnMaxLifetime(5 * time.Minute)
+
+	return &PDB{DB: db}, nil
+}
+
+// Close — корректное закрытие
+func (db *PDB) Close() error {
+	if db == nil || db.DB == nil {
+		return nil
+	}
+	return db.DB.Close()
+}
+
+func (db *PDB) Migrate(path string) error {
+
+	driver, err := postgres.WithInstance(db.DB.DB, &postgres.Config{
+		MigrationsTable: "schema_migrations",
+	})
+	if err != nil {
+		return fmt.Errorf("driver error(postgres): %w", err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://"+path,
+		"postgres",
+		driver,
+	)
+	if err != nil {
+		return fmt.Errorf("migrate init error(postgres): %w", err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("migration error(postgres): %w", err)
+	}
+
+	return nil
+}
